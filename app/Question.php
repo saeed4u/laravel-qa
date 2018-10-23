91636366228d744856b81c7de833604a985ac651
= <=>
2018-10-24 01:25:15 +0300

    Applied scss to dynamically switch between classes

--- app/Question.php
+++ app/Question.php
@@ -15,16 +15,30 @@ class Question extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function setTitleAttribute($value){
+    public function setTitleAttribute($value)
+    {
         $this->attributes['title'] = $value;
-        $this->attributes['slug'] = str_slug($value); 
+        $this->attributes['slug'] = str_slug($value);
     }
 
-    public function getUrlAttribute(){
-        return route('questions.show',$this->id);
+    public function getUrlAttribute()
+    {
+        return route('questions.show', $this->id);
     }
 
-    public function getCreatedDateAttribute(){
+    public function getCreatedDateAttribute()
+    {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id ){
+                return 'answer-accepted';
+            }
+            return 'answered';
+        }
+        return 'unanswered';
     }
 }
